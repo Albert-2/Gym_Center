150e6f3379435b47e79bfff00c1c5f54173690a8
Bhushan <alberteinstein02.99792458>
2023-05-29 15:55:35 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Gym Center
+
+This repository contains source code for gym website created using HTML,tailwind css and js
+
+## Features
+
+- Resposive design
+- Fullscreen mode
+- Can use as a template
+
+## Installation
+
+To clone this project follow the steps
+
+    1.First copy https url from code dropdown
+
+    2.Open git bash and change the current working directory to the location where you want the cloned directory.
+
+    3.type git clone and past the http url
+
+```bash
+  $ git clone https://github.com/USERNAME/REPOSITORY
+```
+
+## 🔗 Links
+
+To see the live preview of project
+
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+## Contributing
+
+If you have any better style or suggestion then please do submit a pull request or open an issue in this repository
+Contributions are always welcome!
---
+++ script.js
@@ -0,0 +1,46 @@
+function onToggle(event) {
+  event.name = event.name === "menu" ? "close" : "menu";
+  document.querySelector(".menu").classList.toggle("top-[9%]");
+  document.querySelector(".menu").classList.toggle("bg-white");
+  document.querySelector(".menu").classList.toggle("text-orange-600");
+}
+const childs = document.querySelectorAll(".child");
+
+childs.forEach(function (divElement) {
+  divElement.addEventListener("mouseover", function () {
+    divElement.classList.add("bg-white", "text-black");
+  });
+
+  divElement.addEventListener("mouseout", function () {
+    divElement.classList.remove("bg-white", "text-black");
+  });
+});
+
+const div2 = document.querySelector("#one");
+
+div2.addEventListener("mouseover", function () {
+  document.querySelector(".effect1").classList.add("bg-orange-600");
+});
+div2.addEventListener("mouseout", function () {
+  document.querySelector(".effect1").classList.remove("bg-orange-600");
+});
+
+const div4 = document.querySelector("#two");
+
+div4.addEventListener("mouseover", function () {
+  document.querySelector(".effect2").classList.add("bg-orange-600");
+});
+
+div4.addEventListener("mouseout", function () {
+  document.querySelector(".effect2").classList.remove("bg-orange-600");
+});
+
+const div6 = document.querySelector("#three");
+
+div6.addEventListener("mouseover", function () {
+  document.querySelector(".effect3").classList.add("bg-orange-600");
+});
+
+div6.addEventListener("mouseout", function () {
+  document.querySelector(".effect3").classList.remove("bg-orange-600");
+});
