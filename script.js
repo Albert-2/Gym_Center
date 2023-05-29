ad568e37d1c5ec43a937a9a6408f46f1b7d86de9
Bhushan <alberteinstein02.99792458>
2023-05-29 20:44:14 +0530

    Fixing mobile-view menu dropdown

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function onToggle(event) {
   event.name = event.name === "menu" ? "close" : "menu";
-  document.querySelector(".menu").classList.toggle("top-[9%]");
+  document.querySelector(".menu").classList.toggle("top-[10%]");
   document.querySelector(".menu").classList.toggle("bg-white");
   document.querySelector(".menu").classList.toggle("text-orange-600");
 }
